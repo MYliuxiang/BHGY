931b6ef3c0edfd95bfad3f6a0faf518d6175ae0d
MYliuxiang <719444625@qq.com>
2020-07-02 14:15:49 +0800

    Initial Commit

---
+++ BHGY/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BHGY
+//
+//  Created by liuxiang on 2020/7/2.
+//  Copyright © 2020 liuxiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ BHGY/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BHGY
+//
+//  Created by liuxiang on 2020/7/2.
+//  Copyright © 2020 liuxiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
